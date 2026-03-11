d58d3a90eab74b35e0db8c46747865ef359aeebd
catelli <eu@gabrielcatelli.com>
2026-03-11 03:56:19 -0300

    feat(actions): test pipeline

--- android/src/main/java/br/com/oititec/rn/sdk/strategy/FacetecStrategy.kt
+++ android/src/main/java/br/com/oititec/rn/sdk/strategy/FacetecStrategy.kt
@@ -1,25 +1,24 @@
-package br.com.oititec.rn.sdk.strategy
-
+package br.com.oititec.sdk.flutter.oiti_sdk.strategy
 
 import android.content.Context
-import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.facetecsdk.domain.model.FacetecManagerOptions
 import br.com.oiti.manager.exports.LivenessResult
+import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.manager.main.CertifaceSDK
-import br.com.oititec.rn.sdk.factories.FacetecThemeFactory
-import com.facebook.react.bridge.ReadableMap
+import br.com.oititec.sdk.flutter.oiti_sdk.factories.FacetecThemeFactory
+import br.com.oiti.manager.main.LivenessProvider
 
 class FacetecStrategy : LivenessProviderStrategy {
   override fun start(
     context: Context,
     appKey: String,
     isCustom: Boolean,
-    theme: ReadableMap?,
+    theme: Map<String, Any>?,
     callback: CertifaceResultCallback<LivenessResult>
   ) {
     val facetecTheme = FacetecThemeFactory.create(isCustom, theme, context)
     val opts = FacetecManagerOptions(appKey, facetecTheme)
-    val mgr = CertifaceSDK.createLivenessManager(CertifaceSDK.LivenessProvider.FACETEC)
-    mgr.start(opts, callback)
+    val manager = CertifaceSDK.createLivenessManager(LivenessProvider.FACETEC)
+    manager.start(opts, callback)
   }
 }
--- android/src/main/java/br/com/oititec/rn/sdk/strategy/IProovStrategy.kt
+++ android/src/main/java/br/com/oititec/rn/sdk/strategy/IProovStrategy.kt
@@ -1,24 +1,24 @@
-package br.com.oititec.rn.sdk.strategy
+package br.com.oititec.sdk.flutter.oiti_sdk.strategy
 
 import android.content.Context
-import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.iproov.domain.model.IProovManagerOptions
 import br.com.oiti.manager.exports.LivenessResult
+import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.manager.main.CertifaceSDK
-import br.com.oititec.rn.sdk.factories.IProovThemeFactory
-import com.facebook.react.bridge.ReadableMap
+import br.com.oiti.manager.main.LivenessProvider
+import br.com.oititec.sdk.flutter.oiti_sdk.factories.IProovThemeFactory
 
 class IProovStrategy : LivenessProviderStrategy {
   override fun start(
     context: Context,
     appKey: String,
     isCustom: Boolean,
-    theme: ReadableMap?,
+    theme: Map<String, Any>?,
     callback: CertifaceResultCallback<LivenessResult>
   ) {
     val iproovTheme = IProovThemeFactory.create(isCustom, theme, context)
     val opts = IProovManagerOptions(appKey, iproovTheme)
-    val manager = CertifaceSDK.createLivenessManager(CertifaceSDK.LivenessProvider.IPROOV)
+    val manager = CertifaceSDK.createLivenessManager(LivenessProvider.IPROOV)
     manager.start(opts, callback)
   }
 }
