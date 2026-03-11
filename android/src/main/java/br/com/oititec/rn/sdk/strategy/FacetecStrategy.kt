96097fd181e128140ea440104a0c3778445b4bbd
catelli <eu@gabrielcatelli.com>
2026-03-11 04:11:41 -0300

    feat(actions): test pipeline

--- android/src/main/java/br/com/oititec/rn/sdk/strategy/FacetecStrategy.kt
+++ android/src/main/java/br/com/oititec/rn/sdk/strategy/FacetecStrategy.kt
@@ -1,19 +1,20 @@
-package br.com.oititec.sdk.flutter.oiti_sdk.strategy
+package br.com.oititec.rn.sdk.strategy
 
 import android.content.Context
+import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.facetecsdk.domain.model.FacetecManagerOptions
 import br.com.oiti.manager.exports.LivenessResult
-import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.manager.main.CertifaceSDK
-import br.com.oititec.sdk.flutter.oiti_sdk.factories.FacetecThemeFactory
 import br.com.oiti.manager.main.LivenessProvider
+import br.com.oititec.rn.sdk.factories.FacetecThemeFactory
+import com.facebook.react.bridge.ReadableMap
 
 class FacetecStrategy : LivenessProviderStrategy {
   override fun start(
     context: Context,
     appKey: String,
     isCustom: Boolean,
-    theme: Map<String, Any>?,
+    theme: ReadableMap?,
     callback: CertifaceResultCallback<LivenessResult>
   ) {
     val facetecTheme = FacetecThemeFactory.create(isCustom, theme, context)
--- android/src/main/java/br/com/oititec/rn/sdk/strategy/IProovStrategy.kt
+++ android/src/main/java/br/com/oititec/rn/sdk/strategy/IProovStrategy.kt
@@ -1,19 +1,20 @@
-package br.com.oititec.sdk.flutter.oiti_sdk.strategy
+package br.com.oititec.rn.sdk.strategy
 
 import android.content.Context
+import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.iproov.domain.model.IProovManagerOptions
 import br.com.oiti.manager.exports.LivenessResult
-import br.com.oiti.domain.callback.CertifaceResultCallback
 import br.com.oiti.manager.main.CertifaceSDK
 import br.com.oiti.manager.main.LivenessProvider
-import br.com.oititec.sdk.flutter.oiti_sdk.factories.IProovThemeFactory
+import br.com.oititec.rn.sdk.factories.IProovThemeFactory
+import com.facebook.react.bridge.ReadableMap
 
 class IProovStrategy : LivenessProviderStrategy {
   override fun start(
     context: Context,
     appKey: String,
     isCustom: Boolean,
-    theme: Map<String, Any>?,
+    theme: ReadableMap?,
     callback: CertifaceResultCallback<LivenessResult>
   ) {
     val iproovTheme = IProovThemeFactory.create(isCustom, theme, context)
